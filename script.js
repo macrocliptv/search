7f55bb550c97a60cd9eb753b482fcf2b2d033c4c
Sebastian Doe <sebastiandoe5@gmail.com>
2019-10-12 13:43:56 +0100

    Make it work

---
+++ script.js
@@ -0,0 +1,6 @@
+let input;
+
+function searchRedirect() {
+    input = document.getElementById('input').value;
+    window.open("https://www.youtube.com/channel/UCAA-SuYx7BCM6O4cAwNsN9Q/search?query=" + input);
+}
